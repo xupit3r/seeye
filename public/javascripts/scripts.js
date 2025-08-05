120431b7f2d60aeb3978f9120f073bb1f9b8d7e0
xupit3r <joe@wahilacreative.com>
2025-08-05 12:17:07 -0400

    apparatus to carry out CA

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,23 +1,48 @@
-const draw = () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  const origin = {
-    x: 25,
-    y: 25
-  };
-  const width = 20;
-  const padding = 2;
-
-  for (let i = 0; i < 10; i++) {
-    const x = origin.x + (width * i) + (padding * i);
-    const y = origin.y;
-
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + width, y);
-    ctx.lineTo(x, y + width);
-    ctx.fill();
+const verifyCanvas = (canvas) => {
+  if (canvas) {
+    return canvas.getContext("2d");
   }
+
+  return false;
 }
 
-window.addEventListener("load", draw);
+const clearCanvas = () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+const rule1 = (cs, ps) => {
+  const ctx = verifyCanvas(document.getElementById("canvas"));
+  const start = 0;
+
+  clearCanvas();
+
+  ctx.beginPath();
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      ctx.quadraticCurveTo(
+        start + i * cs,
+        start + j * cs,
+        start + i * ps,
+        start + j * ps);
+    }
+  }
+  ctx.stroke();
+}
+
+const drawAutomata = () => {
+  let steps = 10;
+  let cs = 2;
+  let ps = 4;
+  const interval = setInterval(() => {
+    if (steps++ > 1000) {
+      return clearInterval(interval);
+    }
+
+    rule1(cs += 1, ps += 1);
+  }, 250);
+}
+
+
+window.addEventListener("load", drawAutomata);
